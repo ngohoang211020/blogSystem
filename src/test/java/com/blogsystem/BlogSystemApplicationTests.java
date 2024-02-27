725f3afe099e78fb4d990d21613c0ddc61eab8e2
BANVIENCORP\hoang.ngo <hoang.ngo@banvien.com.vn>
2024-02-27 13:30:35 +0700

    Init project

---
+++ src/test/java/com/blogsystem/BlogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
