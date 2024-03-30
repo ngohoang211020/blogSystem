90875b7e81c53c6b95294732699785f3636501fa
hoang.ngo <hoanggg2110@gmail.com>
2024-03-30 11:06:31 +0700

    Done github action pipeline for deployement ci cd

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ start=$(date +"%s")
 ssh ${SERVER_USER}@${SERVER_HOST} -i key.pem -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
 cd blogSystem
 git pull
-docker-compose up -d
+docker-compose restart
 exit
 ENDSSH
 
